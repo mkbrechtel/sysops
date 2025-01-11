30ea976417cee229c8133aa2b6f09dcc2ef8f65f
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2025-01-11 03:20:24 +0100

    try to remove trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://patterns.mkbrechtel.dev",
+  trailingSlash: 'never',
   integrations: [
     starlight({
       title: "Cute Patterns!",
